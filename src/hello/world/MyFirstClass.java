c5726002f15838e00eb6d03e626d3854a32629e3
Ezra <ezra.davis@gmail.com>
2013-11-02 15:06:26 -0400

    Creating a repo using eclipse

---
+++ src/hello/world/MyFirstClass.java
@@ -0,0 +1,11 @@
+package hello.world;
+//If I exported a jar called helloworld.jar & opened up terminal,
+//java -classpath helloworld.jar hello.world.MyFirstClass
+// would return 'hello world'
+
+public class MyFirstClass {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+	}
+}
